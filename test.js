20a42c2b08541bbcbb7286870bea8766116da232
A-Isack <61166785+A-Isack@users.noreply.github.com>
2023-12-03 02:50:13 +0200

    .

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 const {get} = require ('https')
 const { faker, da } = require('@faker-js/faker');
-const { parse } = require('path');
+const { start } = require('repl');
 
 let pdata = [
 
@@ -889,7 +889,7 @@ async function bodyTemperature(doctorName, diagnosisId) {
     console.log([min, max])
 }
 
-bodyTemperature('Ashraf', 1)
+// bodyTemperature('Ashraf', 1)
 
 
 async function getdata(){
@@ -906,9 +906,121 @@ async function getdata(){
     })
 
     let data = await getPromise
-    console.log(data.error)
+    console.log(data)
     return data
 }
 
 
 // getdata()
+
+function caesarCipher(s, k) {
+    // Write your code here
+    
+   let alphabet = ['a','b', 'c', 'd', 'e' ,'f', 'g','h','i','j'
+   ,'k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'
+   ]
+
+function rotate(start , k) {
+    let currentIndex = start
+    
+    for (let index = 0; index < k; index++) {
+        currentIndex += 1
+        if(currentIndex === alphabet.length){currentIndex = 0}
+    }
+    console.log(currentIndex)
+    return currentIndex 
+}
+   
+   let newString = ""
+
+   let srtArr = s.split('')
+//    console.log(srtArr)
+   
+   let newStr = ""
+   for(let i=0 ; i<srtArr.length ; i++){
+        let originalChar = srtArr[i]
+        
+
+        let found = false
+        
+        // console.log(originalChar)
+        let upperCase = false
+        if(originalChar == originalChar.toUpperCase()){upperCase = true}
+        
+        alphabet.forEach((alphaChar, idx)=>{
+
+            if(alphaChar === originalChar.toLowerCase()){
+                console.log(idx)
+                    upperCase === true ? 
+                    newStr += alphabet[rotate(idx, k)].toUpperCase() 
+                    : newStr += alphabet[rotate(idx, k)]              
+                found = true
+            }
+        })
+        
+        if(found === false){newStr += originalChar}
+
+    }
+
+    // console.log(newStr)
+    return newStr
+}
+
+// console.log(caesarCipher('www.abc.xy', 4))
+// console.log(caesarCipher('middle-Outz', 2))
+
+
+// console.log("a">"b")
+
+
+let grid = [ 'eabcd', 'fghij', 'olkmn', 'trpqs', 'xywuv' ]
+
+
+function gridChallenge(grid) {
+
+    let sortedGrid = []
+
+    grid.forEach(g=>{
+        
+            sortedGrid.push(g.split('').sort())
+        })
+        
+    console.log(sortedGrid)
+
+    let cols = sortedGrid.length
+    let rows = sortedGrid[0].length
+
+    console.log(rows + '||' + cols)
+    
+    let invArr = []
+    
+    for(let i = 0; i<cols ; i++){
+        invArr.push([])
+    }
+    
+    console.log('invArr')
+    console.log(invArr)
+    
+    invArr.forEach((e,i)=>{
+        sortedGrid.forEach((el,id)=>{
+            invArr[i].push(el[i])
+        })
+    })
+
+    let sorted = "YES"
+    invArr.forEach((e,i)=>{
+        if(i+1 < e.length){
+
+            if(e[i] > e[i+1]){ console.log(e[i] +" || "+ e[i+1]);  sorted = "NO"; return}
+        }
+    })
+
+    console.log(invArr)
+
+    console.log(sorted)
+
+    return sorted
+
+}
+
+gridChallenge(grid)
